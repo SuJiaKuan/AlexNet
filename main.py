dfb7c14b338cfb45ed0a605044a7db86cc94a786
SuJiaKuan <jiakuan.su@toppano.in>
2017-09-01 10:40:26 +0800

    Enable multi processes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from alexnet import AlexNet
 import numpy as np
 import cv2
 import time
+import os
 from tensorflow.python.client import timeline
 
 num_classes = 1000
@@ -13,9 +14,21 @@ train_layers = []
 
 MAX_BATCH_SIZE = 128
 ITER_NUM = 30
+PROCESS_NUM = 1
 
 worker = None
 exp_name = 'native'
+
+# dataset initialize
+dataset = DataSet("./training_data")
+
+# Fork processes
+proc_id = 0
+for idx in range(1, PROCESS_NUM):
+    new_pid = os.fork()
+    if new_pid == 0:
+        proc_id = idx
+        break
 
 # placeholder
 image_ph = tf.placeholder(tf.float32, [None, 256, 256, 3], "x_input_ph")
@@ -26,10 +39,7 @@ model = AlexNet(image_ph, dropout_prob, num_classes, train_layers)
 
 score = model.fc8
 
-# dataset initialize
-dataset = DataSet("./training_data")
-
-
+# Now, run the network
 with tf.Session(worker) as sess:
     tf.global_variables_initializer().run()
 
@@ -40,17 +50,16 @@ with tf.Session(worker) as sess:
       for i in range(ITER_NUM):
           batch = dataset.next_batch(batch_size)
 
-          print("Start inferring... batch_size={} iter={}".format(batch_size, i))
+          print("Start inferring... process={} batch_size={} iter={}".format(proc_id, batch_size, i))
           start_time = time.time()
 
           result = sess.run([score], feed_dict={image_ph: batch[0]})
 
           end_time = time.time()
           inference_time = int(round((end_time - start_time) * 1000))
-          print("Finish inferring... batch_size={} iter={}".format(batch_size, i))
-          print("time cosume: {} ms".format(inference_time))
+          print("Finish inferring... process={}  batch_size={} iter={} time_consume={}ms".format(proc_id, batch_size, i, inference_time))
 
-          with open("result_{}.txt".format(exp_name), "a") as out:
+          with open("result_{}_{}-{}.txt".format(exp_name, PROCESS_NUM, proc_id), "a") as out:
               if i == 0:
                   out.write("batch_size={}\t".format(batch_size))
               out.write("{}\t".format(inference_time))
